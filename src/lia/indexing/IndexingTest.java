102e5b148c038af71e08f7b9fa812f8a539b32a8
donnice <402696415@qq.com>
2016-06-23 17:58:55 -0500

    indexing

--- src/lia/indexing/IndexingTest.java
+++ src/lia/indexing/IndexingTest.java
@@ -19,7 +19,51 @@ import java.io.IOException;
 public class IndexingTest extends TestCase{
 	protected String[] ids = {"1","2"};
 	protected String[] unindexed = {"Netherlands","Italy"};
-	protected String[] unsorted = {"Amsterdam has lots of bridges",
+	protected String[] unstored = {"Amsterdam has lots of bridges",
 			"Venice has lots of canals"};
 	protected String[] text = {"Amsterdam","Venice"};
+	protected Directory dir;
+	
+	protected void setUp() throws IOException{
+		dir = new RAMDirectory();
+		
+		IndexWriter writer = getWriter();
+		
+		for(int i = 0; i < ids.length; i++){
+			Document doc = new Document();
+			doc.add(new Field("id",ids[i],
+						Field.Store.YES,
+						Field.Index.NOT_ANALYZED));
+			doc.add(new Field("country",unindexed[i],
+					Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
+			doc.add(new Field("contents",unstored[i],
+					Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
+			doc.add(new Field("city",text[i],
+					Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
+			writer.addDocument(doc);
+		}
+		writer.close();
+	}
+	
+	private IndexWriter getWriter() throws IOException {
+		return new IndexWriter(dir, new WhitespaceAnalyzer(),
+				IndexWriter.MaxFieldLength.UNLIMITED);
+	}
+	
+	protected int getHitCount(String fieldName, String searchString)
+		throws IOException{
+		IndexSearcher searcher = new IndexSearcher(dir);	// new searcher
+		Term t = new Term(fieldName, searchString);
+		Query query = new TermQuery(t);
+		int hitCount = TestUtil.hitCount(searcher,query);
+		searcher.close();
+		return hitCount;
+	}
+	
+	public void testIndexWriter() throws IOException {
+		Index
+	}
 }
