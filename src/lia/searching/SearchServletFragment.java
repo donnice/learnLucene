09a1b192ef5f2dc818f0198818d33f8c4957f151
Shujie <402696415@qq.com>
2016-07-07 10:42:52 -0500

    Search Servlet Fragment

--- src/lia/searching/SearchServletFragment.java
+++ src/lia/searching/SearchServletFragment.java
@@ -10,6 +10,27 @@ import javax.servlet.http.*;
 import javax.servlet.*;
 import lia.extsearch.queryparser.NumericQueryParserTest.NumericDateRangeQueryParser;
 
-public class SearchServletFragment {
+public class SearchServletFragment extends HttpServlet{
 // Second
+	private IndexSearcher searcher;
+	
+	protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) 
+        throws ServletException, IOException {
+		
+		QueryParser parser = new NumericDateRangeQueryParser(Version.LUCENE_30,
+															 "contents",
+									new StandardAnalyzer(Version.LUCENE_30));
+		parser.setLocale(request.getLocale());
+		parser.setDateResolution(DateTools.Resolution.DAY);
+		
+		Query query = null;
+		try {
+			query = parser.parse(request.getParameter("q"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		TopDocs docs = searcher.search(query, 10);
+	}
 }
