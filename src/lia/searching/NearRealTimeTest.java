22c0887bd1dde148df478e8cd2899f6fe975e53f
Shujie CHEN <shujiec@wolfram.com>
2016-07-07 09:03:07 -0500

    Near Real Time test

---
+++ src/lia/searching/NearRealTimeTest.java
@@ -0,0 +1,14 @@
+package lia.searching;
+
+import org.apache.lucene.util.Version;
+import org.apache.lucene.store.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.document.*;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import junit.framework.TestCase;
+
+public class NearRealTimeTest {
+
+}
