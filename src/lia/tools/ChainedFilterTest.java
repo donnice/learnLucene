e3bf5aa33bfb05dab43bcd993af4849c3f29e585
Shujie <402696415@qq.com>
2016-07-25 09:43:38 -0500

    preparing the filter

--- src/lia/tools/ChainedFilterTest.java
+++ src/lia/tools/ChainedFilterTest.java
@@ -28,5 +28,61 @@ import lia.common.TestUtil;
 
 // From chapter 9
 public class ChainedFilterTest extends TestCase {
+	public static final int MAX = 500;
+	private RAMDirectory directory;
+	private IndexSearcher searcher;
+	private Query query;
+	private Filter dateFilter;
+	private Filter bobFilter;
+	private Filter sueFilter;
 	
+	public void setUp() throws Exception {
+		directory = new RAMDirectory();
+		
+		IndexWriter writer = 
+			new IndexWriter(directory, new WhitespaceAnalyzer(),
+							IndexWriter.MaxFieldLength.UNLIMITED);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2009,1,1,0,0);	// 2009 Jan 01
+		
+		for(int i = 0; i < MAX; i++){
+			Document doc = new Document();
+			doc.add(new Field("key",""+(i+1),
+						Field.Store.YES, Field.Index.NOT_ANALYZED));
+			doc.add(new Field("owner",(i < MAX/2)?"bob":"sue",
+						Field.Store.YES, Field.Index.NOT_ANALYZED));
+			doc.add(new Field("date",DateTools.timeToString(cal.getTimeInMillis(), 
+					DateTools.Resolution.DAY),
+					Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
+			writer.addDocument(doc);
+			cal.add(Calendar.DATE, 1);
+		}
+		
+		writer.close();
+		
+		searcher = new IndexSearcher(directory);
+		
+		BooleanQuery bq = new BooleanQuery();
+		bq.add(new TermQuery(new Term("owner","bob")),
+			   BooleanClause.Occur.SHOULD);
+		bq.add(new TermQuery(new Term("owner","sue")),
+				   BooleanClause.Occur.SHOULD);
+		query = bq;
+		
+		cal.set(2099, 1,1,0,0);
+		
+		// date filter matches all the documents
+		dateFilter = TermRangeFilter.Less("date",
+						DateTools.timeToString(cal.getTimeInMillis(), 
+											   DateTools.Resolution.DAY));
+		bobFilter = new CachingWrapperFilter(
+				new QueryWrapperFilter(
+					new TermQuery(new Term("owner","bob"))));
+		sueFilter = new CachingWrapperFilter(
+				new QueryWrapperFilter(
+					new TermQuery(new Term("owner","sue"))));
+		
+	}
 }
